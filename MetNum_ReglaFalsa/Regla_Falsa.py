7a2ea652216430e23cf1582ce1683f958bfccc7b
Aldowo <A22310381@live.ceti.mx>
2024-11-16 05:13:16 +0000

    Ver2.1

--- MetNum_ReglaFalsa/Regla_Falsa.py
+++ MetNum_ReglaFalsa/Regla_Falsa.py
@@ -3,6 +3,7 @@ from math import *
 from prettytable import PrettyTable
 from math import e
 import math
+
 #Funcion para insertar la expresion algebraica
 def ecuacion():
     expr = input("Ecuacion a evaluar: ")
@@ -14,6 +15,7 @@ def funcion(expr, val):
     y = eval(expr)
     return y
 
+#Funcion que toma los valores de la interseccion
 def valoresI():
     a= float(input("Introduzca el valor de a: "))
     b= float(input("Introduzca el valor de b: "))
@@ -25,25 +27,29 @@ def tolerancia():
     error = error+1
     return error
 
+#Entrada de datos
 fx=ecuacion() #Pide la ecuacion
 Tolerancia=tolerancia() #Pide la tolerancia
-
 
 #Crea la tabla donde se mostraran las posibles intersecciones
 tabla1= PrettyTable(["X","F(X)"])
 tabla1.title="Seleccione los valores a considerar"
+
 #Crea la tabla donde se mostraran las raices
 tabla2= PrettyTable(["No.","a","f(a)","b","f(b)","c","f(c)","ErrorR","%Error"])
 tabla2.title="Metodo de la regla falsa"
+
+#Bucle que evualuca funciones para encontrar las posibles intersecciones
 i1=5
 while(i1>-6):
     fxP=funcion(fx,i1)
     tabla1.add_row([i1,"{0:.7f}".format(fxP)])
     i1=i1-1
+
 print(tabla1)
 
+#Se piden los valores de a y b deseados
 a,b=valoresI()
-
 
 #Se colocan valores iniciales para iniciar el bucle while
 error=1
@@ -65,6 +71,7 @@ while(i<Tolerancia):
     #Se calcula el error relativo
     error=abs((c-cOld)/(c))
     cOld=c
+    
     #Se hace el cambio de posicion segun el signo de la funcion c
     if fc<0:
         a=c
@@ -74,6 +81,7 @@ while(i<Tolerancia):
     tabla2.add_row([i,"{0:.7f}".format(aOld),"{0:.7f}".format(fa),"{0:.7f}".format(bOld),"{0:.7f}".format(fb),
                    "{0:.7f}".format(c),"{0:.7f}".format(fc),"{0:.7f}".format(error),"{0:.7f}".format(error*100)])    
     i=i+1
+
 #Se imprime la tabla al completo
 print(tabla2)
 c="{0:.5f}".format(c)
