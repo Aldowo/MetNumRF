9c96e91a7dca7b82bd1a1bbe1c88b83b81389081
Aldowo <A22310381@live.ceti.mx>
2024-11-27 05:02:33 +0000

    VerFinal

--- MetNum_ReglaFalsa/Regla_Falsa.py
+++ MetNum_ReglaFalsa/Regla_Falsa.py
@@ -33,14 +33,15 @@ def tablaRaiz(fx):
         i1=i1-1
     print(tabla1)
 
-def Metodo(a,b,fx):
-
+def Metodo(a,b,fx,cs):
+    
     tabla2= PrettyTable(["No.","a","f(a)","b","f(b)","c","f(c)","ErrorR","%Error"])
     tabla2.title="Metodo de la regla falsa"
     
     error=1
     i=1
     cOld=0
+
     
     while(i<Tolerancia):
         fa= funcion(fx,a)
@@ -58,14 +59,14 @@ def Metodo(a,b,fx):
             a=c
         else:
             b=c
-
-        tabla2.add_row([i,"{0:.7f}".format(aOld),"{0:.7f}".format(fa),"{0:.7f}".format(bOld),"{0:.7f}".format(fb),
-                    "{0:.7f}".format(c),"{0:.7f}".format(fc),"{0:.7f}".format(error),"{0:.7f}".format(error*100)])    
+    
+        tabla2.add_row([i,round(aOld,cs),round(fa,cs),round(bOld,cs),round(fb,cs),
+                    round(c,cs),round(fc,cs),round(error,cs),round(error*100,cs)])    
         i=i+1
 
     print(tabla2)
-    c="{0:.5f}".format(c)
-    print(f"La raiz es: {c}")
+    Credondo=round(c,cs)
+    print(f"La raiz es: {Credondo}")
     
 
 
@@ -87,8 +88,10 @@ while opc!=0:
         while opc2!=0 and opc2!=2:
             tablaRaiz(x)
             a,b=valoresI()
+            print("Cuantas cifras quiere redondear?: ")
+            fix= int(input())
             Tolerancia=tolerancia() 
-            Metodo(a,b,x)
+            Metodo(a,b,x,fix)
     
             print("Seleccione la operacion a realizar: ")
             print("1)Calcular otra raiz con la misma funcion")
